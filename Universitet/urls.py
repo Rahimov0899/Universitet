fa8db8cfd1ae98108b3ac17c3f24ee5b305f78b2
Rahimov0899 <boburraximov0899@gmail.com>
2023-05-05 23:01:27 +0500

    Malumotlar qo'shish imkonyatlari qo'shildi

--- Universitet/urls.py
+++ Universitet/urls.py
@@ -4,6 +4,10 @@ from asosiy.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ustoz/', ustoz),
-
+    path('ustozlar/', ustozlar),
+    path('bitta_ochir/<int:id>/', ustoz),
+    path('fanlar/', fanlar),
+    path('bitta_ochir/<int:id>/', fan),
+    path('yonalish/', yonalishlar),
+    path('bitta_ochir/<int:id>/', yonalish),
 ]
--- asosiy/views.py
+++ asosiy/views.py
@@ -2,17 +2,86 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import *
 
-def ustoz(request):
-    nom=request.GET.get('qidiruv')
-    if nom=="" or nom is None:
-        content={
-        "ustoz":Ustoz.objects.all()
-    }
+def ustozlar(request):
+    if request.method=='POST':
+        Ustoz.objects.create(
+            ism=request.POST.get('ism'),
+            jins=request.POST.get('j'),
+            yosh=request.POST.get('yosh'),
+            daraja=request.POST.get('d'),
+            fan=Fan.objects.get(id=request.POST.get('fan'))
+
+        )
+        return redirect("/ustozlar/")
+    soz=request.GET.get('qidiruv')
+    if soz=='' or soz is None:
+       content={
+           "ustozlar":Ustoz.objects.all(),
+           "fan":Fan.objects.all()
+
+        }
     else:
+
         content={
-        "ustoz":Ustoz.objects.filter(ism__contains=nom)
-    }
+            "ustozlar":Ustoz.objects.filter(ism__contains=soz)
+        }
     return render(request,"unversitet.html",content)
 
-def tralaba(request):
-    return HttpResponse("<h1>Bobur</h1>")
+
+def ustoz(request,id):
+    Ustoz.objects.get(id=id).delete()
+    return redirect("/ustozlar/")
+
+def fanlar(request):
+    if request.method=='POST':
+        Fan.objects.create(
+            nom=request.POST.get('nom'),
+            asosiy=request.POST.get('asosiy'),
+            yonalish=Yonalish.objects.get(id=request.POST.get('y'))
+
+        )
+        return redirect("/fanlar/")
+    soz=request.GET.get('qidiruv')
+    if soz=='' or soz is None:
+       content={
+           "fanlar":Fan.objects.all(),
+           "yonalish":Yonalish.objects.all()
+
+        }
+    else:
+
+        content={
+            "fanlar":Fan.objects.filter(ism__contains=soz)
+        }
+    return render(request,"fan.html",content)
+
+
+def fan(request,id):
+    Fan.objects.get(id=id).delete()
+    return redirect("/fanlar/")
+
+def yonalishlar(request):
+    if request.method=='POST':
+        Yonalish.objects.create(
+            nom=request.POST.get('nom'),
+            aktiv=request.POST.get('aktiv'),
+
+        )
+        return redirect("/yonalish/")
+    soz=request.GET.get('qidiruv')
+    if soz=='' or soz is None:
+       content={
+           "yonalish":Yonalish.objects.all()
+
+        }
+    else:
+
+        content={
+            "yonalish":Yonalish.objects.filter(ism__contains=soz)
+        }
+    return render(request,"yonalish.html",content)
+
+
+def yonalish(request,id):
+    Yonalish.objects.get(id=id).delete()
+    return redirect("/yonalish/")
